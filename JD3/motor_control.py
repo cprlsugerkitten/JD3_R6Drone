0c40530bc2e03eceef0ddce9e02c3c47c595e2e7
Myles Fernau <CprlSugerKitten@gmail.com>
2024-04-26 17:40:16 -0400

    Add files via upload

--- JD3/motor_control.py
+++ JD3/motor_control.py
@@ -1,72 +1,73 @@
 import RPi.GPIO as GPIO
 import websocket
 import json
+import time
 
-motorA_en = 17
-motorA_forward = 27
-motorA_backward = 22
+#motorA_en = 17
+motorA_forward = 22
+motorA_backward = 27
 
-motorB_en = 5
+#motorB_en = 5
 motorB_forward = 6
 motorB_backward = 13
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(motorA_en, GPIO.OUT)
+#GPIO.setup(motorA_en, GPIO.OUT)
 GPIO.setup(motorA_forward, GPIO.OUT)
 GPIO.setup(motorA_backward, GPIO.OUT)
 
-GPIO.setup(motorB_en, GPIO.OUT)
+#GPIO.setup(motorB_en, GPIO.OUT)
 GPIO.setup(motorB_forward, GPIO.OUT)
 GPIO.setup(motorB_backward, GPIO.OUT)
 
-pwm_A = GPIO.PWM(motorA_en, 1000)
-pwm_B = GPIO.PWM(motorB_en, 1000)
-pwm_A.start(0)
-pwm_B.start(0)
+# pwm_A = GPIO.PWM(motorA_en, 1000)
+# pwm_B = GPIO.PWM(motorB_en, 1000)
+# pwm_A.start(0)
+# pwm_B.start(0)
 
 
 def control_motor(motor, action, speed):
 	if motor == 'A':
-		enable = motorA_en
+		#enable = motorA_en
 		forward = motorA_forward
 		backward = motorA_backward
-		pwm = pwm_A
+		#pwm = pwm_A
 	else:
-		enable = motorB_en
+		#enable = motorB_en
 		forward = motorB_forward
 		backward = motorB_backward
-		pwm = pwm_B
+		#pwm = pwm_B
 
-	pwm.ChangeDutyCycle(speed)
+	#pwm.ChangeDutyCycle(speed)
 
 	if action == 'forward':
-		GPIO.output(motorA_forward, True)
-		GPIO.output(motorB_forward, True)
-		GPIO.output(motorA_backward, False)
-		GPIO.output(motorB_backward, False)
+		GPIO.output(motorA_forward, GPIO.HIGH)
+		GPIO.output(motorB_forward, GPIO.HIGH)
+		GPIO.output(motorA_backward, GPIO.LOW)
+		GPIO.output(motorB_backward, GPIO.LOW)
 	elif action == 'backward':
-		GPIO.output(motorA_forward, False)
-		GPIO.output(motorB_forward, False)
-		GPIO.output(motorA_backward, True)
-		GPIO.output(motorB_backward, True)
+		GPIO.output(motorA_forward, GPIO.LOW)
+		GPIO.output(motorB_forward, GPIO.LOW)
+		GPIO.output(motorA_backward, GPIO.HIGH)
+		GPIO.output(motorB_backward, GPIO.HIGH)
 	elif action == 'right':
-		GPIO.output(motorA_forward, True)
-		GPIO.output(motorB_forward, False)
-		GPIO.output(motorA_backward, False)
-		GPIO.output(motorB_backward, True)
+		GPIO.output(motorA_forward, GPIO.HIGH)
+		GPIO.output(motorB_forward, GPIO.LOW)
+		GPIO.output(motorA_backward, GPIO.LOW)
+		GPIO.output(motorB_backward, GPIO.HIGH)
 	elif action == 'left':
-		GPIO.output(motorA_forward, False)
-		GPIO.output(motorB_forward, True)
-		GPIO.output(motorA_backward, True)
-		GPIO.output(motorB_backward, False)
-	else:
-		GPIO.output(motorA_forward, False)
-		GPIO.output(motorB_forward, False)
-		GPIO.output(motorA_backward, False)
-		GPIO.output(motorB_backward, False)
-		pwm_A.ChangeDutyCycle(0)
-		pwm_B.ChangeDutyCycle(0)
+		GPIO.output(motorA_forward, GPIO.LOW)
+		GPIO.output(motorB_forward, GPIO.HIGH)
+		GPIO.output(motorA_backward, GPIO.HIGH)
+		GPIO.output(motorB_backward, GPIO.LOW)
+	elif action == 'stop':
+		GPIO.output(motorA_forward, GPIO.LOW)
+		GPIO.output(motorB_forward, GPIO.LOW)
+		GPIO.output(motorA_backward, GPIO.LOW)
+		GPIO.output(motorB_backward, GPIO.LOW)
+		#pwm_A.ChangeDutyCycle(0)
+		#pwm_B.ChangeDutyCycle(0)
 	return
 
 def on_message(ws, message):
@@ -84,7 +85,14 @@ def on_close(ws):
 	GPIO.cleanup()
 if __name__ == "__main__":
 	websocket.enableTrace(True)
-	ws = websocket.WebSocketApp("ws://10.0.2.15:3000",
+	#control_motor('A', 'forward', 50)
+	time.sleep(5)
+	#control_motor('A', 'stop', 50)
+	ws = websocket.WebSocketApp("ws://172.20.10.4:8080",
 	on_message = on_message, on_error = on_error, on_close = on_close)
 	print(f"Message: {on_message}, Error: {on_error}, Close: {on_close}")
 	ws.run_forever()
+# if __name__ == "__main__":
+	# with connect("ws://172.20.10.4:8080") as websocket:
+		# message = websocket.recv()
+		
